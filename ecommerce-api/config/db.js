f34e777ca0ebc92016d13bcd06619329f9332f4a
Muhammad Awais Khan <95935530+askhan963@users.noreply.github.com>
2026-03-02 14:11:58 +0500

    Change database connection to local MongoDB

--- ecommerce-api/config/db.js
+++ ecommerce-api/config/db.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-  //  const conn = await mongoose.connect('mongodb://awaiskhan963:awaiskhanniazi@ac-ymeczwc-shard-00-00.jhki4ct.mongodb.net:27017,ac-ymeczwc-shard-00-01.jhki4ct.mongodb.net:27017,ac-ymeczwc-shard-00-02.jhki4ct.mongodb.net:27017/mydatabase?ssl=true&replicaSet=atlas-13au00-shard-0&authSource=admin&retryWrites=true&w=majority', {
    const conn = await mongoose.connect('mongodb://localhost:27017', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
